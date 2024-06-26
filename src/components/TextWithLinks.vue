74d80f869821f5f045a379797a8a7f01513ccf54
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2024-06-26 20:46:12 +0400

    feat: `/faq` page

---
+++ src/components/TextWithLinks.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { type Link } from '~/lib/constants/types'
+defineProps<{ content: (string | Link)[] }>()
+</script>
+
+<template>
+  <template v-for="item in content">
+    <template v-if="typeof item === 'string'">{{ item }}</template>
+    <template v-else>
+      <NuxtLink :href="item.href" :target="item.href.startsWith('https://') ? '_blank' : undefined">
+        {{ item.title }}
+      </NuxtLink>
+    </template>
+  </template>
+</template>
---
+++ src/lib/constants/types.ts
@@ -0,0 +1,5 @@
+export type Link = {
+  title: string
+  href: string
+  target?: '_blank' | '_parent' | '_self' | '_top'
+}
