c18499cf82ffbe598e12c2fbd827901d2e275a2a
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2024-06-26 18:09:31 +0400

    feat: increase spacing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <HeroSection class="pt-3xl" />
   <hr>
-  <BenefitsSection class="my-xxl" />
+  <BenefitsSection class="mt-xxl mb-3xl" />
   <NetworkSection class="my-xxl" />
   <FeaturesSection class="mt-xxl mb-3xl" />
   <BannerSection class="my-3xl" />
